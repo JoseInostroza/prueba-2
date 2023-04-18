7f48a300d5a2582e86430dd900928495ef1d461d
JoseInostroza <87823768+JoseInostroza@users.noreply.github.com>
2023-04-18 13:00:03 -0400

    sass nav responsive

--- assets/js/search.js
+++ assets/js/search.js
@@ -4,8 +4,12 @@ const searchContenedor = document.querySelector('.nav__cuerpo-searchBar')
 
 searchBtn.addEventListener('click', ()=>{
     searchContenedor.classList.add('active')
+    searchBtn.classList.add('active')
+    closeBtn.classList.add('active')
 })
 
 closeBtn.addEventListener('click', ()=>{
     searchContenedor.classList.remove('active')
+    searchBtn.classList.remove('active')
+    closeBtn.classList.remove('active')
 })
